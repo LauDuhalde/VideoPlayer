642cf8688c9c3a96ff6bde70738afa0c47bf1fbc
Laura Duhalde <duhalde.ulloa.laura@gmail.com>
2023-12-12 09:11:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.videoplayer
+
+import android.net.Uri
+import android.net.Uri.parse
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.VideoView
+import android.widget.MediaController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Find the VideoView class by its id
+        val videoView = findViewById<VideoView>(R.id.testView)
+        //Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri: Uri = parse(
+            "android.resource://" + packageName
+                    + "/raw/test"
+        )
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
